a14e70585f6eecec5ac1bf02ea3f412092d375a2
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-08 02:12:35 -0300

    feat: add validators to user model

--- backend/src/users/dto/createUserRequest.ts
+++ backend/src/users/dto/createUserRequest.ts
@@ -1,7 +1,12 @@
+import { IsEmail, IsStrongPassword } from 'class-validator';
 export class CreateUserRequest {
   firstName: string;
   lastName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsStrongPassword()
   password: string;
   passwordConfirmation: string;
 }
