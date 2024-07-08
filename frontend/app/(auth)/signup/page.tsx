e2e4c333a68d7a506f1ec9525e0b1a82ab15d53c
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-08 01:19:03 -0300

    style: change background color and make textInputs horizontal

--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,15 +1,20 @@
-import { Button, Link, Stack, TextField } from "@mui/material";
+import { Button, Divider, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
 
 export default function Login() {
   return (
-    <Stack spacing={4} className="w-full max-w-xs">
-      <div>
+    <Stack
+      spacing={4}
+      className="w-full max-w-lg p-6"
+      sx={{ backgroundColor: "#2b2e30", borderRadius: "20px" }}
+    >
+      <h1 className="text-4xl pb-4 pt-4">Cadastro</h1>
+      <div className="flex space-x-4">
         <TextField label="Primeiro nome" variant="outlined" />
         <TextField label="Sobrenome" variant="outlined" />
       </div>
       <TextField label="Email" variant="outlined" type="email" />
-      <div>
+      <div className="flex space-x-4">
         <TextField label="Senha" variant="outlined" type="password" />
         <TextField
           label="Confirmação de senha"
