0c81842776387c37a83e1d047fde62619ed68117
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-08 00:32:28 -0300

    fix: add return statements to pages

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -2,12 +2,14 @@ import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
 
 export default function Login() {
-  <Stack spacing={4} className="w-full max-w-xs">
-    <TextField label="Email" variant="outlined" type="email" />
-    <TextField label="Senha" variant="outlined" type="password" />
-    <Button variant="contained">Login</Button>
-    <Link component={NextLink} href="/signup" className="self-center">
-      Não tem um login? Faça seu cadastro!
-    </Link>
-  </Stack>;
+  return (
+    <Stack spacing={4} className="w-full max-w-xs">
+      <TextField label="Email" variant="outlined" type="email" />
+      <TextField label="Senha" variant="outlined" type="password" />
+      <Button variant="contained">Login</Button>
+      <Link component={NextLink} href="/signup" className="self-center">
+        Não tem um login? Faça seu cadastro!
+      </Link>
+    </Stack>
+  );
 }
--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -2,23 +2,25 @@ import { Button, Link, Stack, TextField } from "@mui/material";
 import NextLink from "next/link";
 
 export default function Login() {
-  <Stack spacing={4} className="w-full max-w-xs">
-    <div>
-      <TextField label="Primeiro nome" variant="outlined" />
-      <TextField label="Sobrenome" variant="outlined" />
-    </div>
-    <TextField label="Email" variant="outlined" type="email" />
-    <div>
-      <TextField label="Senha" variant="outlined" type="password" />
-      <TextField
-        label="Confirmação de senha"
-        variant="outlined"
-        type="password"
-      />
-    </div>
-    <Button variant="contained">Finalizar cadastro</Button>
-    <Link component={NextLink} href="/login" className="self-center">
-      Já tem uma conta? Faça seu login!
-    </Link>
-  </Stack>;
+  return (
+    <Stack spacing={4} className="w-full max-w-xs">
+      <div>
+        <TextField label="Primeiro nome" variant="outlined" />
+        <TextField label="Sobrenome" variant="outlined" />
+      </div>
+      <TextField label="Email" variant="outlined" type="email" />
+      <div>
+        <TextField label="Senha" variant="outlined" type="password" />
+        <TextField
+          label="Confirmação de senha"
+          variant="outlined"
+          type="password"
+        />
+      </div>
+      <Button variant="contained">Finalizar cadastro</Button>
+      <Link component={NextLink} href="/login" className="self-center">
+        Já tem uma conta? Faça seu login!
+      </Link>
+    </Stack>
+  );
 }
