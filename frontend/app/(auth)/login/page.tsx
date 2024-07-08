a8be9caa38ed54057be62e9d7d01e5f5e759f0a9
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-08 01:30:01 -0300

    style: add background color and title to login page

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -3,7 +3,12 @@ import NextLink from "next/link";
 
 export default function Login() {
   return (
-    <Stack spacing={4} className="w-full max-w-xs">
+    <Stack
+      spacing={4}
+      className="w-full max-w-lg p-6"
+      sx={{ backgroundColor: "#2b2e30", borderRadius: "20px" }}
+    >
+      <h1 className="text-4xl pb-4 pt-4">Login</h1>
       <TextField label="Email" variant="outlined" type="email" />
       <TextField label="Senha" variant="outlined" type="password" />
       <Button variant="contained">Login</Button>
