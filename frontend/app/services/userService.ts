35a2be6892894f897d404fd989b87a6e2b291b8d
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-09 16:38:49 -0300

    fix: pass data do backend

--- frontend/app/services/userService.ts
+++ frontend/app/services/userService.ts
@@ -7,6 +7,7 @@ import { getErrorMessage } from "../utils/errorMessage";
 export default async function createUser(_prevState: any, formData: FormData) {
   const response = await fetch(`${API_URL}/users`, {
     method: "POST",
+    body: formData,
   });
 
   const parsedResponse = await response.json();
