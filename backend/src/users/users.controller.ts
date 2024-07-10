51f07bea30e3069947f4e197b750b7923a978dcf
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-10 01:04:48 -0300

    feat: create current user endpoint

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,7 +1,17 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CreateUserRequest } from './dto/createUserRequest';
 import { UsersService } from './users.service';
 import { NoFilesInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'utils/decorators/current-user.decorator';
+import { TokenPayload } from 'utils/interfaces/token-payload.interface';
 
 @Controller('users')
 export class UsersController {
@@ -11,5 +21,11 @@ export class UsersController {
   @UseInterceptors(NoFilesInterceptor())
   createUser(@Body() request: CreateUserRequest) {
     return this.usersService.createUser(request);
+  }
+
+  @Get('current-user')
+  @UseGuards(JwtAuthGuard)
+  getCurrentUser(@CurrentUser() user: TokenPayload) {
+    return user;
   }
 }
