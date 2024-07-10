f6b989b3078e807e66be24abdc5116eb392cc4a3
Pedro Henrique Viveiros <pedrohr.viveiros@gmail.com>
2024-07-10 02:32:50 -0300

    feat: create Header

---
+++ frontend/app/common/constants/routes.ts
@@ -0,0 +1,17 @@
+export const unauthenticatedRoutes = [
+  {
+    title: "Login",
+    path: "/login",
+  },
+  {
+    title: "Cadastro",
+    path: "/signup",
+  },
+];
+
+export const routes = [
+  {
+    title: "Home",
+    path: "/",
+  },
+];
